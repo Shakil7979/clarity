63d1a9e5a6f90803c65025a516c9c5726cabcc73
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2025-02-17 00:31:58 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,6 +15,28 @@ $(document).ready(function(){
 		autoplay:true,
 	});
 
+	$(".header_area").hover(
+        function() {
+            $(this).addClass("add_header");
+        },
+        function() {
+            $(this).removeClass("add_header");
+        }
+    );
+
+	// $(document).ready(function () {
+	// 	$(".menu_item").hover(
+	// 		function () {
+	// 			$(".mega_menu").css("left", "0"); // Slide in from the left
+	// 		},
+	// 		function () {
+	// 			$(".mega_menu").css("left", "-100%"); // Hide to the left
+	// 		}
+	// 	);
+	// });
+	
+	
+
 
 
 
